97c8c5f343b7569e83cc20513173b3109b90ecd5
ttbachyinsda <zjkgfz@sina.com>
2016-10-16 21:36:03 +0800

    Add some logs.

--- database/databasehandler/table.h
+++ database/databasehandler/table.h
@@ -13,26 +13,26 @@ class Table
 public:
     Table();
     Table(string name,string filename);
-    string getname();
-    string getfilename();
-    DataBaseType* getcolumn(int i);
-    string getcolumnname(int i);
-    bool setname(string name);
-    void setfilename(string filename);
-    bool checkInsert(vector<string> data);
-    vector<string> checkOutput();
-    virtual bool Insert()=0;
-    virtual bool Initialize()=0;
+    string getname();               //return the name of table
+    string getfilename();           //return the filename of table
+    DataBaseType* getcolumn(int i); //return the selected column of table
+    string getcolumnname(int i);    //return the name of the selected column of table
+    bool setname(string name);      //set the name of table
+    void setfilename(string filename);  //set the filename of table
+    bool checkInsert(vector<string> data);  //Check if data is correct, and cache it.
+    vector<string> checkOutput();           //Check if data is correct, and output it.
+    virtual bool Insert()=0;                //Insert the data in the cache
+    virtual bool Initialize()=0;            //Initialize table
     bool havecreatetable=false;
     bool haveinitialize=false;
-    virtual int getPageNum()=0;
-    virtual int getPageRowNum(int pagenum)=0;
-    virtual bool Modify(int pagenum,int rownum)=0;
-    virtual bool DeleteAt(int pagenum,int rownum)=0;
-    virtual bool FindAt(int pagenum,int rownum)=0;
-    virtual void createTable(vector<string> clname,vector<string> cltype,vector<int> clsize)=0;
-    vector<string> gettype();
-    virtual ~Table();
+    virtual int getPageNum()=0;             //return the num of pages that table contains.
+    virtual int getPageRowNum(int pagenum)=0; //return the number of rows in the selected page.
+    virtual bool Modify(int pagenum,int rownum)=0; //modify a record
+    virtual bool DeleteAt(int pagenum,int rownum)=0; //delete a record
+    virtual bool FindAt(int pagenum,int rownum)=0;      //find a record and cache it
+    virtual void createTable(vector<string> clname,vector<string> cltype,vector<int> clsize)=0; //create a table
+    vector<string> gettype();   //return columns information
+    virtual ~Table();           //destroy table, don't forget to execute it.
 protected:
     void clearcolumn();
     string name;
