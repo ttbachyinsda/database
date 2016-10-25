0044487683a0f0389c82d426cdca0b9ffca21d94
JerryJohn <jzynoter@gmail.com>
2016-10-25 11:25:39 +0800

    修改key大小中

--- DBIndex/predefined.h
+++ DBIndex/predefined.h
@@ -11,34 +11,32 @@ namespace bpt {
 /* key/value type */
 typedef int value_t;
 struct key_t {
-    char k[16];
+  char k[32];
 
-    key_t(const char *str = "")
-    {
-        bzero(k, sizeof(k));
-        strcpy(k, str);
-    }
+  key_t(const char *str = "") {
+    bzero(k, sizeof(k));
+    strcpy(k, str);
+  }
 };
 
 inline int keycmp(const key_t &a, const key_t &b) {
-    int x = strlen(a.k) - strlen(b.k);
-    return x == 0 ? strcmp(a.k, b.k) : x;
+  int x = strlen(a.k) - strlen(b.k);
+  return x == 0 ? strcmp(a.k, b.k) : x;
 }
 
-#define OPERATOR_KEYCMP(type) \
-    bool operator< (const key_t &l, const type &r) {\
-        return keycmp(l, r.key) < 0;\
-    }\
-    bool operator< (const type &l, const key_t &r) {\
-        return keycmp(l.key, r) < 0;\
-    }\
-    bool operator== (const key_t &l, const type &r) {\
-        return keycmp(l, r.key) == 0;\
-    }\
-    bool operator== (const type &l, const key_t &r) {\
-        return keycmp(l.key, r) == 0;\
-    }
-
+#define OPERATOR_KEYCMP(type)                                                  \
+  bool operator<(const key_t &l, const type &r) {                              \
+    return keycmp(l, r.key) < 0;                                               \
+  }                                                                            \
+  bool operator<(const type &l, const key_t &r) {                              \
+    return keycmp(l.key, r) < 0;                                               \
+  }                                                                            \
+  bool operator==(const key_t &l, const type &r) {                             \
+    return keycmp(l, r.key) == 0;                                              \
+  }                                                                            \
+  bool operator==(const type &l, const key_t &r) {                             \
+    return keycmp(l.key, r) == 0;                                              \
+  }
 }
 
 #endif /* end of PREDEFINED_H */
